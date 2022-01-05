b3e8d2aca50658c0960fc5bf733fa21e0c81cb3e
kang4609 <kang4609@gmail.com>
2022-01-05 22:04:49 +0900

    Initial commit

---
+++ src/main/java/hello/hellospringproject/HelloSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellospringproject/HelloSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
